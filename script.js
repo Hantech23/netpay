7eae883824a27fe31e2ceb203be025da4e9d32f7
Woobin Han <woobin.han23@gmail.com>
2023-10-08 19:54:19 -0400

    added actualRate and hypothetical pay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let totalHours = 31.5;
+let totalHours = 41;
 const medicare = 0.0145;
 const socialSecurity = 0.062;
 const federalIncomeTax = 0.04867725;
@@ -7,3 +7,12 @@ let totalPay = totalHours*rate;
 console.log(totalPay);
 let netPay = totalPay - (totalPay*medicare) - (totalPay*socialSecurity) - (totalPay*federalIncomeTax);
 console.log(netPay);
+
+const totalTax = medicare + socialSecurity + federalIncomeTax;
+13.1025
+
+const actualRate = rate - (.1265*rate);
+console.log(actualRate*totalHours + " actual rate calc")
+
+let hypothetical = 60*actualRate;
+console.log(hypothetical)
