2c468d5c2c3ae13034520bc840e8ebfd2b59766d
Woobin Han <woobin.han23@gmail.com>
2023-11-11 13:35:09 -0500

    Added and linked payrate and total hour fields. Added result display.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-let totalHours = 41;
+
 const medicare = 0.0145;
 const socialSecurity = 0.062;
 const federalIncomeTax = 0.04867725;
-const rate = 15;
-let totalPay = totalHours*rate;
-console.log(totalPay);
-let netPay = totalPay - (totalPay*medicare) - (totalPay*socialSecurity) - (totalPay*federalIncomeTax);
-console.log(netPay);
+
+
+
+
+
+
+//console.log("Total pay: " + totalPay);
+//let netPay = totalPay - (totalPay*medicare) - (totalPay*socialSecurity) - (totalPay*federalIncomeTax);
+//console.log("Net pay: " + netPay);
 
 const totalTax = medicare + socialSecurity + federalIncomeTax;
 13.1025
 
-const actualRate = rate - (.1265*rate);
-console.log(actualRate*totalHours + " actual rate calc")
+//const actualRate = rate - (.1265*rate);
+//console.log(actualRate*totalHours + " actual rate calc")
 
-let hypothetical = 60*actualRate;
-console.log(hypothetical)
+//let hypothetical = 80*actualRate;
+//console.log("Hypothetical: " + hypothetical)
+
+document.getElementById("submit").onclick = function(event) {
+    event.preventDefault()
+    let rate = document.getElementById("rateInput").value;
+    let totalHours = document.getElementById("totalHoursInput").value;
+    let totalPay = totalHours*rate;
+    let netPay = totalPay - (totalPay*medicare) - (totalPay*socialSecurity) - (totalPay*federalIncomeTax);
+    console.log("Netpay: " + netPay)
+    let resultOutput = document.createElement("p")
+    resultOutput.textContent = "Netpay: " + netPay
+    document.body.appendChild(resultOutput)
+}
